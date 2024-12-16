8e9649d292c5c3827c659117b8cf21f508d059c6
SamBroomy <36888606+SamBroomy@users.noreply.github.com>
2024-12-16 11:02:38 +0000

    day-1 both problems

--- crates/day-1/src/main.rs
+++ crates/day-1/src/main.rs
@@ -1,10 +1,8 @@
 use anyhow::Result;
 use common::get_input;
 
-fn day_1() -> Result<i32> {
-    let input = get_input(1)?;
-
-    let (mut left, mut right): (Vec<i32>, Vec<i32>) = input
+fn parse_input(input: &str) -> (Vec<i32>, Vec<i32>) {
+    input
         .lines()
         .filter_map(|line| {
             let line = line.split_whitespace().collect::<Vec<&str>>();
@@ -13,20 +11,37 @@ fn day_1() -> Result<i32> {
             let second = line.last()?.parse::<i32>().ok()?;
             Some((first, second))
         })
-        .unzip();
+        .unzip()
+}
+
+fn part_1(input: &str) -> i32 {
+    let (mut left, mut right) = parse_input(input);
     left.sort();
     right.sort();
 
-    let total = left
-        .into_iter()
+    left.into_iter()
         .zip(right.into_iter())
-        .fold(0, |acc, (l, r)| acc + (l - r).abs());
-    println!("{}", total);
-    Ok(total)
+        .fold(0, |acc, (l, r)| acc + (l - r).abs())
+}
+
+fn part_2(input: &str) -> i32 {
+    let (left, right) = parse_input(input);
+
+    left.iter()
+        .map(|l| l * right.iter().filter(|r| l == *r).count() as i32)
+        .fold(0, |acc, x| acc + x)
 }
 
 fn main() -> Result<()> {
-    day_1().map(|_| ())
+    let input = get_input(1)?;
+
+    let p1 = part_1(&input);
+    println!("Part1: {}", p1);
+
+    let p2 = part_2(&input);
+    println!("Part2: {}", p2);
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -34,8 +49,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_day_one() {
-        let total = day_1().unwrap();
+    fn test_parse_input() {
+        let input = "1   2\n3   4\n5   6";
+        let (left, right) = parse_input(input);
+        assert_eq!(left, vec![1, 3, 5]);
+        assert_eq!(right, vec![2, 4, 6]);
+    }
+
+    #[test]
+    fn test_day_1_part_1() {
+        let input = get_input(1).unwrap();
+        let total = part_1(&input);
         assert_eq!(total, 1579939);
+    }
+
+    #[test]
+    fn test_day_2_part_2() {
+        let input = get_input(1).unwrap();
+        let total = part_2(&input);
+        assert_eq!(total, 20351745);
     }
 }
