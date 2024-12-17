b6195e197e9f8dcc88a780292224bee8cd98467a
SamBroomy <36888606+SamBroomy@users.noreply.github.com>
2024-12-17 22:21:55 +0000

    day 5 part 2

--- crates/day-5/src/main.rs
+++ crates/day-5/src/main.rs
@@ -1,11 +1,10 @@
 use anyhow::Result;
 use common::get_input;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
-fn parse_input(input: &str) -> (HashMap<i32, HashSet<i32>>, Vec<Vec<i32>>) {
+fn parse_input(input: &str) -> (HashSet<(i32, i32)>, Vec<Vec<i32>>) {
     let (page_ordering_rules, pages_to_produce) = input.split_once("\n\n").unwrap();
-    let mut page_ordering_rules_hash_map = HashMap::new();
-    page_ordering_rules
+    let rules = page_ordering_rules
         .lines()
         .filter_map(|line| {
             line.split_once("|").and_then(|(lhs, rhs)| {
@@ -14,18 +13,7 @@ fn parse_input(input: &str) -> (HashMap<i32, HashSet<i32>>, Vec<Vec<i32>>) {
                 Some((lhs, rhs))
             })
         })
-        .for_each(|(l, r)| {
-            page_ordering_rules_hash_map
-                .entry(l)
-                .and_modify(|e: &mut HashSet<i32>| {
-                    e.insert(r);
-                })
-                .or_insert_with(|| {
-                    let mut set = HashSet::new();
-                    set.insert(r);
-                    set
-                });
-        });
+        .collect::<HashSet<_>>();
 
     let pages_to_produce_vec = pages_to_produce
         .lines()
@@ -36,40 +24,49 @@ fn parse_input(input: &str) -> (HashMap<i32, HashSet<i32>>, Vec<Vec<i32>>) {
         })
         .collect::<Vec<Vec<i32>>>();
 
-    (page_ordering_rules_hash_map, pages_to_produce_vec)
+    (rules, pages_to_produce_vec)
+}
+
+fn sorted(x: &i32, y: &i32, rules: &HashSet<(i32, i32)>) -> bool {
+    !rules.contains(&(*y, *x))
+}
+
+fn compare(x: &i32, y: &i32, rules: &HashSet<(i32, i32)>) -> std::cmp::Ordering {
+    if rules.contains(&(*x, *y)) {
+        return std::cmp::Ordering::Less;
+    } else if rules.contains(&(*y, *x)) {
+        return std::cmp::Ordering::Greater;
+    } else {
+        return std::cmp::Ordering::Equal;
+    }
 }
 
 fn part_1(input: &str) -> i32 {
-    let (page_ordering_rules, pages_to_produce) = parse_input(input);
+    let (rules, pages_to_produce) = parse_input(input);
 
-    let mut count = 0;
-
-    'outer: for pages in pages_to_produce.iter() {
-        // Loop over in reverse order, don't need to check the last page since it has no preceding pages
-        for i in (1..pages.len()).rev() {
-            // if the page has no proceeding page rules, skip
-            let Some(invalid_preceding_pages) = page_ordering_rules.get(&pages[i]) else {
-                continue;
-            };
-            let preceding_pages = pages
-                .get(0..i)
-                .unwrap()
-                .iter()
-                .cloned()
-                .collect::<HashSet<i32>>();
-            // if the page has any invalid preceding pages, these pages are invalid
-            if invalid_preceding_pages
-                .intersection(&preceding_pages)
-                .count()
-                != 0
-            {
-                continue 'outer;
+    pages_to_produce
+        .into_iter()
+        .map(|pages| {
+            if pages.is_sorted_by(|a, b| sorted(a, b, &rules)) {
+                pages[pages.len() / 2]
+            } else {
+                0
             }
-        }
-        // If we reach this point, all pages are valid so we can find the center page number and total it to the count
-        count += pages[pages.len() / 2];
-    }
-    count
+        })
+        .sum()
+}
+
+fn part_2(input: &str) -> i32 {
+    let (rules, pages_to_produce) = parse_input(input);
+
+    pages_to_produce
+        .into_iter()
+        .filter(|pages| !pages.is_sorted_by(|a, b| sorted(a, b, &rules)))
+        .map(|mut pages| {
+            pages.sort_by(|a, b| compare(a, b, &rules));
+            pages[pages.len() / 2]
+        })
+        .sum()
 }
 
 fn main() -> Result<()> {
@@ -77,8 +74,8 @@ fn main() -> Result<()> {
     let p1 = part_1(&input);
     println!("Part1: {}", p1);
 
-    // let p2 = part_2(&input);
-    // println!("Part2: {}", p2);
+    let p2 = part_2(&input);
+    println!("Part2: {}", p2);
 
     Ok(())
 }
@@ -92,5 +89,12 @@ mod tests {
         let input = get_input(5).unwrap();
         let total = part_1(&input);
         assert_eq!(total, 7024);
+    }
+
+    #[test]
+    fn day_5_part_2() {
+        let input = get_input(5).unwrap();
+        let total = part_2(&input);
+        assert_eq!(total, 4151);
     }
 }
