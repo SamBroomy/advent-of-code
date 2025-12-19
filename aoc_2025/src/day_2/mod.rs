9d661c007feb046fad686442d3d0d330d9f1af28
SamBroomy <36888606+SamBroomy@users.noreply.github.com>
2025-12-19 22:37:19 +0000

    fixed test

--- aoc_2025/src/day_2/mod.rs
+++ aoc_2025/src/day_2/mod.rs
@@ -1,23 +1,11 @@
 fn parse(input: &str) -> impl Iterator<Item = u64> + '_ {
-    input.split(',').flat_map(|i| {
+    input.trim().split(',').flat_map(|i| {
         let (first, second) = i.split_once('-').unwrap();
         let first = first.parse().unwrap();
         let second = second.parse().unwrap();
         first..=second
     })
 }
-
-// fn check_equal_parts(num: u64, part_len: u32, parts: u32) -> bool {
-//     let div = 10_u64.pow(part_len);
-//     let first_part = num / div.pow(parts - 1);
-//     for i in 1..parts {
-//         let current_part = (num / div.pow(parts - 1 - i)) % div;
-//         if current_part != first_part {
-//             return false;
-//         }
-//     }
-//     true
-// }
 
 #[inline]
 pub fn part1(input: &str) -> u64 {
@@ -78,5 +66,4 @@ pub fn part2(input: &str) -> u64 {
         .sum()
 }
 
-common::aoc_test!(1227775554, 31000881061, 4174379265, 46769308485
-);
+common::aoc_test!(1227775554, 31000881061, 4174379265, 46769308485);
